918f50bb57575caf847f4904b5118d88831baf67
Aydin Uzmez <aydin@uzmez.net>
2019-11-18 12:12:51 +0300

    **fix:** if the file already has been deleted. error is returning. fix it

--- pyinstaller/clear_pyinstaller.py
+++ pyinstaller/clear_pyinstaller.py
@@ -2,8 +2,18 @@ import shutil
 import os
 
 try:
-    os.remove("quickConvert.spec")
     shutil.rmtree("build")
     shutil.rmtree("dist")
-except FileNotFoundError as e:
-    print("Already deleted")
+    os.remove("quickConvert.spec")
+except WindowsError as e:
+    print("{0} already has been deleted".format(e.filename))
+
+
+try:
+    shutil.rmtree("build")
+    shutil.rmtree("dist")
+    os.remove("install_regedit.spec")
+
+except WindowsError as e:
+    print("{0} already has been deleted".format(e.filename))
+
